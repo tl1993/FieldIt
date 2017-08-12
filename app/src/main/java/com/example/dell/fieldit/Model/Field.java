fcb3929a00a1eae4253740d80f0812e4f8cfcc1f
tl1993 <tomer.laniado@gmail.com>
2017-08-12 18:43:47 +0300

    Field class updated

--- app/src/main/java/com/example/dell/fieldit/Model/Field.java
+++ app/src/main/java/com/example/dell/fieldit/Model/Field.java
@@ -16,11 +16,16 @@ public class Field {
     private String id;
     private String name;
     private String type;
+    private String userId;
     private double longitude;
     private double latitude;
     private String description;
+    private String imageName;
+    private boolean isLighted;
+    private double lastUpdated;
+    private boolean isDeleted;
 
-    public Field(String id,String name,String type,double latitude,double longitude,String description)
+    public Field(String id,String name,String type,double latitude,double longitude,String description,boolean isLighted)
     {
         this.id = id;
         this.name = name;
@@ -28,14 +33,33 @@ public class Field {
         this.description=description;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.isLighted = isLighted;
     }
-
+    // Getters
     public  String getId(){return this.id;}
     public  String getName() { return this.name;}
     public  String getType() {return this.type;}
     public double getLongitude(){return this.longitude;}
     public double getLatitude() {return this.latitude;}
     public String  getDescription() {return this.description;}
+    public String getImageName() { return this.imageName;}
+    public String getUserId() { return this.userId;}
+    public boolean getIslighted() { return this.isLighted;}
+    public double getLastUpdated() { return this.lastUpdated;}
+    public boolean getIsDeleted() {return this.isDeleted;}
+    // Setters
+    public void setId(String id) { this.id = id;}
+    public void setName(String name) { this.name = name;}
+    public void setType(String type) { this.type = type;}
+    public void setLongitude(double longitude) { this.longitude = longitude;}
+    public void setLatitude(double latitude) { this.latitude = latitude;}
+    public void setDescription(String description) { this.description = description;}
+    public void setImageName(String imageName){ this.imageName = imageName;}
+    public void setLastUpdated(double lastUpdated) { this.lastUpdated = lastUpdated;}
+    public void setIsDeleted(boolean isDeleted){ this.isDeleted = isDeleted;}
+    public void setUserId(String userId) { this.userId = userId;}
+    public void setIsLighted(boolean isLighted){this.isLighted = isLighted;}
+
      public Map<String, Object> toMap() {
          HashMap<String, Object> result = new HashMap<>();
          result.put("name",this.name);
@@ -43,6 +67,8 @@ public class Field {
          result.put("description",this.description);
          result.put("latitude",this.latitude);
          result.put("longitude",this.longitude);
+         result.put("user_id",this.userId);
+         result.put("is_lighted",this.isLighted);
          return result;
      }
 }
